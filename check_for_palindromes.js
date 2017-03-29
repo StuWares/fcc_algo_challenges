35ad753bedda5cebaf9c19b97c16eaeed593c70c
Stu Wares <stuwares@outlook.com>
2017-03-29 20:53:29 +0100

    added blank file for palindromes challenge

---
+++ check_for_palindromes.js
@@ -0,0 +1,5 @@
+// coded by Stu Wares, twitter.com/TeamRocketGB
+
+// Return true if the given string is a palindrome. Otherwise, return false.
+
+// convert the sting to an array, cut it in half, reverse one then compare?
