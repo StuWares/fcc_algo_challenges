f2feeb7f964dc9207b361ae062ffae4687f68f5f
Stu Wares <stuwares@outlook.com>
2017-03-31 07:30:13 +0100

    added map to create array of string lengths

--- longest_word.js
+++ longest_word.js
@@ -3,3 +3,24 @@
 // Return the length of the longest word in the provided sentence.
 
 // Your response should be a number.
+
+/* TODO split the string by spaces, iterate across the array, checking length
+making a new array of numbers (length values), arrange sting numerically
+.pop off the end and return*/
+
+function findLongestWord(str) {
+	var arrayedWords = [];
+	var wordLengths = [];
+
+	arrayedWords = str.split(" ");
+
+	wordLengths = arrayedWords.map(function (val)) {
+		return val.length;
+	});
+
+
+
+  return str.length;
+}
+
+findLongestWord("The quick brown fox jumped over the lazy dog");
