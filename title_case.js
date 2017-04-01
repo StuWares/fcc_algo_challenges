632c6d7242b1bac6a39507f59396377ebea3b4e8
Stu Wares <stuwares@outlook.com>
2017-04-01 23:36:54 +0100

    added solution for title case challenge

--- title_case.js
+++ title_case.js
@@ -6,10 +6,27 @@ each word capitalized. Make sure the rest of the word is in lower case.
 For the purpose of this exercise, you should also capitalize connecting
 words like "the" and "of". */
 
+/* original version used more variables, rather than dot notation.
+ changed it as I need to get into the habit of coding that way!*/
 
+ /* realised I don't actually need any variables here, all of this could
+ be done straight on the str argument.  Didn't change it, left it as
+ a lesson for myself! */
 
 function titleCase(str) {
-  return str;
+
+	var lowerCase = [];
+
+
+	lowerCase = str.toLowerCase().split(" ") // turns str into an array, all lower case
+
+	.map(function (val) {
+		return (val.charAt(0).toUpperCase + val.slice(1)); // .charAt(0) targets first letter for toUpperCase
+	})
+
+	.join(" ");
+
+	return titleCased;
 }
 
 titleCase("I'm a little tea pot");
