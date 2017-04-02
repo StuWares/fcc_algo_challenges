300647385fb1bc1889d7eb00d42d8b8a5b7f3b7f
Stu Wares <stuwares@outlook.com>
2017-04-02 20:46:13 +0100

    adding the solution for largest numbers challenge

--- largest_numbers_arrays.js
+++ largest_numbers_arrays.js
@@ -4,3 +4,22 @@
 Return an array consisting of the largest number from each provided sub-array.
 For simplicity, the provided array will contain exactly 4 sub-arrays.
  */
+
+/* .map .sort then pop? */
+
+function largestOfFour(arr) {
+
+    arr.map(function (val){
+        val.sort(function (a, b){
+            return a - b;
+        });
+     });
+    
+    arr = arr.map(function (val){   // arr = arr.map, otherwise arr.map alone returns the arrays without the last index
+      return val.pop();
+    });
+    
+return arr;
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
