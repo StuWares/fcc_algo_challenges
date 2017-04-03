5d651274089b8d8e1b45e3b803ef11c4d8f1f55b
Stu Wares <stuwares@outlook.com>
2017-04-03 08:29:34 +0100

    adding solution for confirm the ending challenge

--- confirm_the_ending.js
+++ confirm_the_ending.js
@@ -7,3 +7,15 @@ This challenge can be solved with the .endsWith() method, which was introduced i
 But for the purpose of this challenge, we would like you to use one of the JavaScript substring 
 methods instead.
 */
+
+// str.length - target.length is the key to this one?
+
+unction confirmEnding(str, target) {
+  var strLength = str.length;
+  var targetLength = target.length;
+  var strEnd = str.substr(-target.length, target.length);
+  
+  return strEnd = target;
+}
+
+confirmEnding("Bastian", "n");
