aedb42b631e5b0cc42bf67bbcc331db00b4c52d0
Stu Wares <stuwares@outlook.com>
2017-04-03 22:47:45 +0100

    adding solution to truncate a string challenge

--- truncate_a_string.js
+++ truncate_a_string.js
@@ -9,3 +9,21 @@ However, if the given maximum string length num is less than or equal to 3,
 then the addition of the three dots does not add to the string length in determining
 the truncated string. */
 
+/* check for:
+num is eaqual to or more than str.length - return str
+num is 3 or less - return truncated string plus ...
+num is greater than 3 - return truncated string including ... */
+
+
+function truncateString(str, num) {
+  
+	if (num >= str.length) {
+		return str;
+	} else if (num <= 3) {
+		return str.slice(0, num) + "...";
+	} else {
+		return str.slice(0, num - 3) + "...";
+	} 
+}
+
+truncateString("A-tisket a-tasket A green and yellow basket", 3);
