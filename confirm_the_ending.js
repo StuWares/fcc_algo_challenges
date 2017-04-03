d814ff192861c8d5d752a4a2175844bb5dc8e39d
Stu Wares <stuwares@outlook.com>
2017-04-03 08:11:18 +0100

    adding blank file for confimr the ending challenge

---
+++ confirm_the_ending.js
@@ -0,0 +1,9 @@
+// coded by Stu Wares, twitter.com/TeamRocketGB
+
+// Check if a string (first argument, str) ends with the given target string (second argument, target).
+
+/*
+This challenge can be solved with the .endsWith() method, which was introduced in ES2015.
+But for the purpose of this challenge, we would like you to use one of the JavaScript substring 
+methods instead.
+*/
