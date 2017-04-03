a11533b3d8f33ac0b9fc6394685f6871fb3dceeb
Stu Wares <stuwares@outlook.com>
2017-04-03 22:03:56 +0100

    adding solution for repeat a string challenge

--- repeat_a_string.js
+++ repeat_a_string.js
@@ -1,1 +1,22 @@
 // coded by Stu Wares, twitter.com/TeamRocketGB
+
+/* Repeat a given string (first argument) num times (second argument).
+Return an empty string if num is not a positive number. */
+
+function repeatStringNumTimes(str, num) {
+  
+	var repeatStr = "";
+
+	if (num <= 0) {			// returns empty string if val is not a positive
+		return repeatStr;
+	} else {
+		for (i = 0; i < num; i++) {			// simple for loop seemed the best here
+			repeatStr = repeatStr + str;
+		}
+	}
+
+
+  return repeatStr;
+}
+
+repeatStringNumTimes("abc", 3);
