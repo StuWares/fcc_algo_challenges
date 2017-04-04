6bc2e1ee34c157b5206ae145d197c7a6c7b739cc
Stu Wares <stuwares@outlook.com>
2017-04-04 15:59:34 +0100

    tried some dead ends for chunky monkey

--- chunky_monkey.js
+++ chunky_monkey.js
@@ -4,48 +4,19 @@
 into groups the length of size (second argument) and returns
 them as a two-dimensional array. */
 
-/* slice arr size times and push the return value into a new array
-map or for? */
+/* slice arr size times and push the return value into a new array*/
 
 
-
-function chunkArrayInGroups(arr, size) {
-  var choppedUp = [];
-
-  
-  	choppedUp[i].push(function (val) {
-      // for loop which iterates at size and slices
-    });  
-
-
-  return choppedUp;
-}
-
-chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3);
-
-
-
-
-
-
-
-
-/* *** wrong approach***
- works unless size is greater than the possible number
-of arrays 
+// works unless size is greater than the array, returns empty arrays
 function chunkArrayInGroups(arr, size) {
   var choppedUp = [];
 
   for (i = 0; i < size; i++) {
-    if (arr === []) {
-      return choppedUp;
-    } else {
-  	choppedUp[i] = arr.splice(0, size);
-    }
+  	choppedUp.push(arr.splice(0, size));
   }
 
 
   return choppedUp;
 }
 
-chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3); */
+chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3); 
