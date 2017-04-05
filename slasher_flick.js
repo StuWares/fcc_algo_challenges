5e0bb99257333715893a183678895e01572dcd58
Stu Wares <stuwares@outlook.com>
2017-04-05 15:49:41 +0100

    adding blank file for slasher flick challenge

---
+++ slasher_flick.js
@@ -0,0 +1,16 @@
+// coded by Stu Wares, twitter.com/TeamRocketGB
+
+/* Return the remaining elements of an array after chopping off n elements from the head.
+
+The head means the beginning of the array, or the zeroth index.*/
+
+// straightforward slice/splice?
+
+function slasher(arr, howMany) {
+  
+
+  
+  return arr;
+}
+
+slasher([1, 2, 3], 2);
