d3000df1188584cbab54977a4845b2f7074b34b8
Stu Wares <stuwares@outlook.com>
2017-04-06 16:14:38 +0100

    added for loop to set all of the arrays to lower case

--- mutations.js
+++ mutations.js
@@ -12,8 +12,25 @@ string "hello" does not contain a "y".
 Lastly, ["Alien", "line"], should return true because all of the
 letters in "line" are present in "Alien". */
 
+/* 
+more complex than I'd originally thought!
+need to compare each letter individually?
+
+set all to lower case
+str.indexOf(searchValue[, fromIndex])
+*/
+
+
 function mutation(arr) {
-  return arr;
+  var mutated = [];
+  
+  
+
+  for (var i = 0; i < arr.length; i++) {
+  	mutated.push(arr[i].toLowerCase());
+  }
+  
+  
 }
 
-mutation(["hello", "hey"]);
+mutation(["Hello", "hey"]);
