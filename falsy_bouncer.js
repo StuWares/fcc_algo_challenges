88f8307161912cfbaf872bf4f3238ad6d24c9127
Stu Wares <Stu Wares>
2017-04-09 20:12:59 +0100

    testing something with vs code on windows

--- falsy_bouncer.js
+++ falsy_bouncer.js
@@ -1,1 +1,12 @@
 // coded by Stu Wares, twitter.com/TeamRocketGB
+
+ /* Remove all falsy values from an array.
+
+Falsy values in JavaScript are false, null, 0, "", undefined, and NaN. */
+
+function bouncer(arr) {
+  
+  return arr;
+}
+
+bouncer([7, "ate", "", false, 9]);
