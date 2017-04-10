80745c13977b96aa2a15f66a64bd83d8ea46e2db
Stu Wares <stuwares@outlook.com>
2017-04-10 15:29:11 +0100

    adding solution for falsy bouncer challenge, using Boolean function

--- falsy_bouncer.js
+++ falsy_bouncer.js
@@ -5,8 +5,9 @@
 Falsy values in JavaScript are false, null, 0, "", undefined, and NaN. */
 
 function bouncer(arr) {
-  
-  return arr;
+ 
+	return arr.filter(Boolean);
+
 }
 
 bouncer([7, "ate", "", false, 9]);
